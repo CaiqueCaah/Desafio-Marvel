52333b0899fc2a95df366dbf7636423ec463b822
Caique Gonçalves da Silva <56288657+CaiqueCaah@users.noreply.github.com>
2024-04-12 00:14:46 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', redirectTo: '/hero', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
